bc8d3573e9898acd8f8088895c438815f1d7e866
Oleksiy Hryshchenko <greeschenko@gmail.com>
2017-02-21 20:09:27 +0200

    up

--- assets/js/contentautoup.js
+++ assets/js/contentautoup.js
@@ -1,18 +1,23 @@
+var CAUget = function(el, url) {
+    $.ajax({
+        url: url,
+        type: 'GET',
+        dataType: 'html',
+        success: function(data, textStatus, jqXHR) {
+            el.html(data);
+        },
+    });
+};
+
 $(window).ready(function() {
     $('.contentautoupwrap').each(function(index) {
         var el = $(this);
         var url = $(this).data('url');
         var interval = $(this).data('interval');
 
+        CAUget(el, url);
         setInterval(function() {
-            $.ajax({
-                url: url,
-                type: 'GET',
-                dataType: 'html',
-                success: function(data, textStatus, jqXHR) {
-                    el.html(data);
-                },
-            });
+            CAUget(el, url);
         }, interval);
     });
 });
